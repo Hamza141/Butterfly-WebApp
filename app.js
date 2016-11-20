8dfddfb267731879b626e1ee55712b68b8f3ca87
trankq <trankq@purdue.edu>
2016-11-20 05:28:24 -0500

    connect to clearDB and local mysql

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
 var express = require("express"),
     app = express(),
 	port = process.env.VCAP_APP_PORT || 8080;
+var mysql = require('mysql');
+/*var connection = mysql.createConnection({
+	host     : 'localhost',
+	user     : 'root',
+	password : 'Ghost999',
+	database : 'Butterfly'
+});*/
+var connection = mysql.createConnection({
+	host     : 'us-cdbr-iron-east-04.cleardb.net',
+	user     : 'b3bd94cf7c5125',
+	password : 'b256d9ed',
+	database : 'ad_9ddc31f035745cd'
+});
+connection.connect(function(err) {
+  console.log('connected to local');
+  var post  = {firstName: 'He', googleID: 'asdf@google.com'};
+  var query = connection.query('INSERT INTO Users SET ?', post, function(err, result) {
+    // Neat!
+  });
+  console.log(query.sql);
+});
+
 app.get("/", function (request, response) {
     response.writeHead(200, {"Content-Type": "text/html"})
     response.end("Index\n");
@@ -8,5 +30,9 @@ app.get("/", function (request, response) {
 app.get("/hello", function (request, response) {
     response.writeHead(200, {"Content-Type": "text/html"})
     response.end("Hello World!\n");
+});
+app.put("/addUser", function (request, response) {
+    response.writeHead(200, {"Content-Type": "text/html"})
+    response.end("Done\n");
 });
 app.listen(port);
