551866d619580738d90347e07b0b50bd5d9510a0
Muhammad Hamza Farrukh <mfarrukh@purdue.edu>
2016-12-03 17:57:45 -0500

    Very basic app is working now

---
+++ Butterfly/routes/index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    res.sendFile(path.join(__dirname, '../', 'views', 'index.html'));
+});
+
+router.get('/data', function (req, res) {
+    res.json([{"id": 1, "name": "Mymm", "message": "Pantano do Sul"},
+        {"id": 2, "name": "Skyble", "message": "Guilmaro"},
+        {"id": 3, "name": "Tagfeed", "message": "Gnosjö"},
+        {"id": 4, "name": "Realcube", "message": "Jrashen"},
+        {"id": 5, "name": "Bluejam", "message": "Zhangjiawo"},
+        {"id": 6, "name": "Jayo", "message": "Obonoma"},
+        {"id": 7, "name": "Cogidoo", "message": "Sungsang"},
+        {"id": 8, "name": "Avavee", "message": "Diawara"},
+        {"id": 9, "name": "Tagtune", "message": "Monywa"},
+        {"id": 10, "name": "Centimia", "message": "Retkovci"}]);
+});
+
+module.exports = router;
