114b02833b23e838a8f56cf7e62c4123de012ecd
trankq <trankq@purdue.edu>
2016-12-03 18:32:41 -0500

    new stuff

--- app.js
+++ app.js
@@ -4,18 +4,19 @@ var http = require('http');
 var querystring = require('querystring');
 var utils = require('utils');
 var mysql = require('mysql');
-/*var db_config = {
+var app = express();
+var db_config = {
 	host     : 'localhost',
 	user     : 'root',
 	password : 'Ghost999',
-	database : 'Butterfly'
-};*/
-var db_config = {
+	database : 'Butterfly-Web'
+};
+/*var db_config = {
 	host     : 'us-cdbr-iron-east-04.cleardb.net',
 	user     : 'b3bd94cf7c5125',
 	password : 'b256d9ed',
 	database : 'ad_9ddc31f035745cd'
-};
+};*/
 var connection;
 function handleDisconnect() {
   	connection = mysql.createConnection(db_config);
@@ -35,40 +36,46 @@ function handleDisconnect() {
   	});
 }
 handleDisconnect();
-var app = express();
 app.set('port', process.env.PORT || 8000);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.get("/", function (request, response) {
 	console.log("[200] " + request.method + " to " + request.url);
 	response.writeHead(200, "OK", {'Content-Type': 'text/html'});
-	response.write('<html><head><title>Hello Noder!</title></head><body>');
-	response.write('<h1>Welcome Noder, who are you?</h1>');
+	response.write('<html><head><title>Chat Box</title></head><body>');
+	response.write('<h1>Name and Message</h1>');
 	response.write('<form enctype="application/x-www-form-urlencoded" action="/formhandler" method="post">');
-	response.write('Name: <input type="text" name="username" value="John Doe" /><br />');
-	response.write('Age: <input type="text" name="userage" value="99" /><br />');
+	response.write('Name: <input type="text" name="username" value="" /><br />');
+	response.write('Message: <input type="text" name="message" value="" /><br />');
 	response.write('<input type="submit" />');
 	response.write('</form></body></html');
 	response.end();
 });
 app.post("/formhandler", function (request, response) {
 	console.log("[200] " + request.method + " to " + request.url);
-	var fullBody = '';
-	request.on('data', function(chunk) {
-		console.log("Received body data:");
-		console.log(chunk.toString());
-		fullBody += chunk.toString();
-		response.end();
-	});
-	request.on('end', function() {
-		response.writeHead(200, "OK", {'Content-Type': 'text/html'});
-		var decodedBody = querystring.parse(fullBody);
-		response.write('<html><head><title>Post data</title></head><body><pre>');
-		response.write(utils.inspect(decodedBody));
-		response.write('</pre></body></html>');
-		response.end();
-	});
-	response.write('<html><head><title>Post data</title></head><body><pre>');
+	var username = request.body.username;
+	var message = request.body.message;
+	connection.connect(function(err) {
+  		console.log('connected to local');
+  		var post  = {username: username, message: message};
+  		var query = connection.query('INSERT INTO Messages SET ?', post, function(err, result) {});
+   		console.log(query.sql);
+ 	});
+	response.writeHead(200, "OK", {'Content-Type': 'text/html'});
+	response.write('<html><head><title>Chat Box</title></head><body>');
+	response.write('<h1>Name and Message</h1>');
+	response.write('<p>message</p>');
+	connection.connect(function(err) {
+  		console.log('connected to local');
+  		var query = connection.query('SELECT * FROM Messages', function(err, result) {
+			console.log(result);
+			for (var i = 0; i < result.length; i++) {
+  				console.log(result[i].message);
+			};
+		});
+   		console.log(query.sql);
+ 	});
+	response.write('</body></html');
 	response.end();
 });
 http.createServer(app).listen(app.get('port'), '0.0.0.0', function() {
